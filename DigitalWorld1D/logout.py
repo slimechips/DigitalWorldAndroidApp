725c37173c0e93f8ef361ac73b074450f772a822
slimechips <jason.chowcs#gmail.com>
2019-04-22 14:39:09 +0800

    fix to missing class

--- DigitalWorld1D/logout.py
+++ DigitalWorld1D/logout.py
@@ -1,6 +1,9 @@
 from kivy.uix.screenmanager import Screen
 import appdimens 
 
+class LogoutBottomNavBar(Screen):
+    pass
+
 class Logout_Confirm(Screen):
     _button_font_size = appdimens.button_font_size
     _title_font_size = appdimens.title_font_size
